b6001f06f2b23b71c1cbd828b519d6af2acc540e
Alicia Eugster S Kantonsschule Glarus <alicia.eugster@stud.schulegl.ch>
2022-09-15 14:32:34 +0200

    added hexadecimal

---
+++ dezimal2binary.py
@@ -0,0 +1,61 @@
+
+
+
+teiler = input("Zahl?")
+
+#decimal -> binary
+binarylist = []
+
+#decimal -> hexadecimal
+hdList = []
+
+integerX = int(teiler)
+zahlB = integerX
+zahlH = integerX
+
+#Falls input < teiler = tauschen
+if zahlB <= 2:
+    teiler = zahlB
+    zahlB = 2
+if zahlH <= 16:
+    teilerH = zahlH
+    zahlH = 16
+
+#Teilungsreste berechnen + Listen hinzufügen
+while zahlB > 0:
+    restB = zahlB % 2
+    zahlB = zahlB // 2
+    binarylist.insert(0, restB)
+print (binarylist)
+
+while zahlH > 0:
+    restH = zahlH % 16
+    if restH >= 9:
+        if restH == 10:
+            restH = "A"
+        elif restH == 11:
+            restH = "B"
+        elif restH == 12:
+            restH = "C"
+        elif restH == 13:
+            restH = "D"
+        elif restH == 14:
+            restH = "E"
+        elif restH == 15:
+            restH = "F"
+    hdList.insert(0, restH)
+    zahlH = zahlH // 16
+    
+print (hdList)
+
+#Output
+outputB = ''
+for i in binarylist:
+    outputB += str(i)
+print(integerX, 'as binary number is', outputB)
+
+outputH = ''
+for i in hdList:
+    outputH += str(i)
+print(integerX, 'as hexadecimal number is', outputH)
+
